8a159672559ee278ecb4f00e85f903488f99cb5f
andymil <andrew.j.milson@gmail.com>
2014-06-21 14:10:26 +1200

    changed Shifty Spritz description and firefox label.

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -2,8 +2,8 @@ var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 
 var button = buttons.ActionButton({
-  id: "mozilla-link",
-  label: "Visit Mozilla",
+  id: "shifty-spritz-popup",
+  label: "Shifty Spritz",
   icon: {
     "16": "./shifty-spritz-16.png",
     "32": "./shifty-spritz-32.png",
